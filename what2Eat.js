011fa6c8266a7fdb1525af1612e5920686d77243
Kornthip <sang.kornthip@gmail.com>
2021-03-28 17:52:22 -0400

    debug any cuisine type search :hamburger: :sushi: :spaghetti: :curry:

--- what2Eat.js
+++ what2Eat.js
@@ -21,21 +21,36 @@ what2EatApp.init = () => {
         what2EatApp.ingredientValue = what2EatApp.ingredientElement.value;
         what2EatApp.cuisineElement = document.querySelector('select');
         what2EatApp.cuisineValue = what2EatApp.cuisineElement.value;
+        console.log(what2EatApp.cuisineValue);
     
-        // pass new search 
-        what2EatApp.getRecipes(what2EatApp.ingredientValue);
+        // pass new search
+        
+        what2EatApp.getRecipes(what2EatApp.ingredientValue,what2EatApp.cuisineValue);
     });
 }
 
 //method to fetch a response from the API with new search params from user
-what2EatApp.getRecipes = (ingredients) => {
+
+what2EatApp.getRecipes = (ingredients,cuisine) => {
     const url = new URL(what2EatApp.apiUrl);
-    url.search = new URLSearchParams({
-        q: ingredients,
-        // cuisineType : cuisine,
-        app_id: what2EatApp.apiId,
-        app_key: what2EatApp.apiKey,
-    });
+    if ( cuisine === "any"){
+        url.search = new URLSearchParams({
+            // populate Query (q) without cuisine type (any cuisine type)
+            q: ingredients,
+            app_id: what2EatApp.apiId,
+            app_key: what2EatApp.apiKey,
+        });
+    } else {
+        url.search = new URLSearchParams({
+            // populate Query (q) and cuisine type from user submission
+            q: ingredients,
+            cuisineType : cuisine,
+            app_id: what2EatApp.apiId,
+            app_key: what2EatApp.apiKey,
+        });
+    }
+    
+    // fetch a response from the API with search params from user
     fetch(url)
     .then(function (response) {
         return response.json();
@@ -45,21 +60,22 @@ what2EatApp.getRecipes = (ingredients) => {
         const recipeOptionsArray = jsonResponse.hits;
         console.log(recipeOptionsArray);
 
+        // filter by health label and put in each array ie.vegan, nut-free, gluten-free, dairy-free
         //filtered array of vegan recipes
         const veganArray = what2EatApp.filterResults(recipeOptionsArray,"Vegan");
-        console.log(veganArray);
         //filtered array of nut-free recipes
         const nutFreeArray = what2EatApp.filterResults(what2EatApp.filterResults(recipeOptionsArray,"Peanut-Free"),"Tree-Nut-Free");
-        console.log(nutFreeArray);
         //filtered array of gluten-free recipes
         const glutenFreeArray = what2EatApp.filterResults(recipeOptionsArray,"Gluten-Free");
-        console.log(glutenFreeArray);
         //filtered array of dairy-free recipes
         const dairyFreeArray = what2EatApp.filterResults(recipeOptionsArray,"Dairy-Free");
-        console.log(dairyFreeArray);
         
         //append recipes
         what2EatApp.showRecipeCard(recipeOptionsArray);
+    }).catch(()=>{
+        //clear previous search
+        what2EatApp.ulElement.innerHTML = "<p>not found</p>";
+        console.log('not getting recipe')
     });
 }
 
@@ -68,38 +84,30 @@ what2EatApp.filterResults = (array,label) => {
     return array.filter(arrayObject => arrayObject.recipe.healthLabels.includes(label)); 
 }    
 
-
-
 //function to append results
 what2EatApp.showRecipeCard = (array) => {
     //clear previous search
     what2EatApp.ulElement.innerHTML = "";
+    // create loop for each recipe
     array.forEach((recipeObject) => {
+        // create elements for responses
         const recipeCard = document.createElement('li');
+        const {url,image,label,calories,ingredientLines,yield} = recipeObject.recipe;
         recipeCard.innerHTML = `
             <div>
-                <a href=${recipeObject.recipe.url} target="_blank">
-                <img src=${recipeObject.recipe.image} alt=${recipeObject.recipe.label}/>
+                <a href=${url} target="_blank">
+                <img src=${image} alt=${label}/>
                 </a>
             </div>
-            <h3>${recipeObject.recipe.label}</h3>
-            <p>calories : ${Math.floor(recipeObject.recipe.calories)}</p>
-            <p>${recipeObject.recipe.ingredientLines.length} ingredients</p>
-            <p>serves: ${recipeObject.recipe.yield}</p>
+            <h3>${label}</h3>
+            <p>${Math.floor(calories)} calories</p>
+            <p>${ingredientLines.length} ingredients</p>
+            <p>serves: ${yield}</p>
         `;
-        //append li to ul
+        // append elements to display on page
         what2EatApp.ulElement.append(recipeCard);
     }); 
 }
-// populate Query (q) section and cuisine type with user submission variable(s)
-// fetch a response from the API with new search params from user
-// create loop for each response
-// filter by health label and put in each array ie. vegan, nut-free, gluten-free, dairy-free
-// create elements for responses
-// append elements to display on page
-
-
-
 
 // call what2EatApp.init
 what2EatApp.init();
